940433fc2e74a9d89b9970141b8433070dd5db1d
fahadsha <44699572+fahadsha@users.noreply.github.com>
2018-11-07 12:17:06 +0100

    Add files via upload

---
+++ inc.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 11:53:37 2018
+
+@author: fahads99
+"""
+
+def inc(userinput):
+    return userinput+1
+
+print(inc(5))
---
+++ incdemo_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 12:06:21 2018
+
+@author: fahads99
+"""
+import unittest 
+from inc import inc
+
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(inc(0), 1)
+        self.assertEqual(inc(100), 101)
+        self.assertEqual(inc(1), 2)
+        self.assertEqual(inc(-1), 0)
+        self.assertEqual(inc(4), 4)
+        
+        
+if __name__ == '__main__':
+    unittest.main()   
